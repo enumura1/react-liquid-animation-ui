d5a5b05094d1bb6593d3fbd936510de9438c4a34
enumura1 <akirayuto0718@gmail.com>
2023-12-14 23:22:56 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+// 外部ファイルのインポート
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="liquid-ui-left"></div>
+        <div className="liquid-ui-center"></div>
+        <div className="liquid-ui-right"></div>
+      </div>
+    </>
+  )
+}
+
+export default App;
